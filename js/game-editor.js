9e5890df271910759bae28ba60e8915b24fbcb62
ASoozay <asousa2021@gmail.com>
2026-05-28 14:53:00 -0700

    css/js

--- js/game-editor.js
+++ js/game-editor.js
@@ -148,7 +148,7 @@ async function loadWriters() {
 //             (2) containerId: container id to determine tab refresh
 // #region openeditGameModal //
 async function openEditGameModal(game, containerId) {
-    currGameId = gameId;
+    currGameId = game.gameId;
     currentContainer = containerId;
 
     await loadSports("edit-sport-input");
