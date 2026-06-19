e9f1a631129e55e58929867beec3ccd57d049650
ASoozay <asousa2021@gmail.com>
2026-06-18 20:31:28 -0700

    make all schools in schoos load

--- js/schools.js
+++ js/schools.js
@@ -6,145 +6,47 @@ let filterContainer;
 console.log(filterContainer);
 
 window.onload = async function() {
-    filterContainer = document.getElementById("schools-filter-sections-container");
-
-    buildSchoolsFilterContainer("School");
-}
-
-
-
-async function buildSchoolsFilterContainer(filterType) {
-    //await loadSports();
-
-    filterContainer.innerHTML = "";
-
-    if(filterType === "School"){
-        buildSchoolsFiltersSchools();
-    }
-
-    if(filterType === "Sport") {
-        //buildSportFilters();
-    }   
-
-    if(filterType === "Conference") {
-        //buildConferenceFilters();
-    }
-}
-
-async function buildSchoolsFiltersSchools() {
-    const selectSchoolFilterSchoolContainer = document.createElement("div");
-    selectSchoolFilterSchoolContainer.className = "schools-filter-section-container";
-
-    const selectSchoolFilterSchoolTitle = document.createElement("div");
-    selectSchoolFilterSchoolTitle.className = "schools-filter-section-title";
-    selectSchoolFilterSchoolTitle.innerHTML = "Select A School";
-    selectSchoolFilterSchoolContainer.append(selectSchoolFilterSchoolTitle);
-
-    const selectSchoolFilterSchoolSearchContainer = document.createElement("div");
-    selectSchoolFilterSchoolSearchContainer.className = "schools-filter-section-search-container";
-    selectSchoolFilterSchoolContainer.append(selectSchoolFilterSchoolSearchContainer);
-
-    const selectSchoolFilterSchoolSearch = document.createElement("input");
-    selectSchoolFilterSchoolSearch.className = "schools-filter-section-search";
-    selectSchoolFilterSchoolSearchContainer.append(selectSchoolFilterSchoolSearch);
-
-    const selectSchoolFilterSchoolSearchResultsContainer = document.createElement("div");
-    selectSchoolFilterSchoolSearchResultsContainer.className = "schools-filter-section-search-results";
-    selectSchoolFilterSchoolSearchContainer.appendChild(selectSchoolFilterSchoolSearchResultsContainer);
-
-    selectSchoolFilterSchoolSearch.addEventListener("input", () => {
-        const search = selectSchoolFilterSchoolSearch.value.trim().toLowerCase();
-
-        selectSchoolFilterSchoolSearchResultsContainer.innerHTML = "";
-
-        if (!search) return;
-
-        const matches = schools
-            .filter(school =>
-                school.school.toLowerCase().startsWith(search)
-            )
-            .slice(0, 10); 
-
-        matches.forEach(school => {
-            const option = document.createElement("div");
-
-            option.classList.add("schools-filter-search-option");
-            option.textContent = school.school;
-
-            option.addEventListener("click", () => {
-                selectSchoolFilterSchoolSearch.value = school.school;
-
-                selectedSchool = school; // <-- save whole object
-
-                selectSchoolFilterSchoolSearchResultsContainer.innerHTML = "";
-            });
-
-            selectSchoolFilterSchoolSearchResultsContainer.appendChild(option);
-        });
-    });
-
-    filterContainer.appendChild(selectSchoolFilterSchoolContainer);
-
-    const selectSchoolFilterSchoolSportContainer = document.createElement("div");
-    selectSchoolFilterSchoolSportContainer.className = "schools-filter-section-container";
-
-    const selectSchoolFilterSchoolSportTitle = document.createElement("div");
-    selectSchoolFilterSchoolSportTitle.className = "schools-filter-section-title";
-    selectSchoolFilterSchoolSportTitle.innerHTML = "Select Sports";
-    selectSchoolFilterSchoolSportContainer.append(selectSchoolFilterSchoolSportTitle);
-
-    const selectSchoolFilterSchoolSportOptionsContainer = document.createElement("div");
-    selectSchoolFilterSchoolSportOptionsContainer.className = "schools-filter-section-sports-container";
-    selectSchoolFilterSchoolSportContainer.append(selectSchoolFilterSchoolSportOptionsContainer);
-    
-    const response = await fetch("/.netlify/functions/get-sport-info", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" }
-    });
-
-    if (!response.ok) {
-        console.log("Failed to fetch sport info. Status:", response.status);
-        return;
-    }
-
+    const response = await fetch("/.netlify/functions/get-schools");
     const data = await response.json();
-    const sports = data.sports;
+    schools = data.schools;   
 
-    sports.forEach(sport => {
-        const optionContainer = document.createElement("div");
-        optionContainer.className = "schools-filter-checkbox-container";
+    showAllSchools();
+}
 
-        const checkbox = document.createElement("option");
-        checkbox.className = "schools-filter-checkbox";
-        checkbox.value = sport.sport;
-        optionContainer.append(checkbox);
+async function showAllSchools() {
+    const schoolContainer = document.getElementById("schools-container");
 
-        const sportName = document.createElement("div");
-        sportName.className = "schools-filter-checkbox-name";
-        sportName.innerHTML = sport.abbreviation;
-        optionContainer.append(sportName);
+    schools.forEach(school => {
+        const schoolBox = createSchoolBox(school, sections = ["school", "acronym", "nickname", "conference", "location"]);
 
-        selectSchoolFilterSchoolSportOptionsContainer.append(optionContainer);
+        schoolContainer.appendChild(schoolBox);
     });
-
-    filterContainer.append(selectSchoolFilterSchoolSportContainer);
 }
 
-async function fetchSchools(filter) {
-    await loadSchools();
+function createSchoolBox(school, sections = []) {
+    const schoolBox = document.createElement("div");
+    schoolBox.className = "school-box";
 
-    if(filter === "conferences");
+    const schoolLogoContainer = document.createElement("div");
+    schoolLogoContainer.className = "school-logo-container";
+    schoolLogoContainer.innerHTML = `<img class = "school-logo" src = "/images/schools/${school.school}.webp">`;
+    schoolBox.appendChild(schoolLogoContainer);
+
+    let divider = document.createElement("div");
+    divider.className = "school-box-divider";
+    schoolBox.appendChild(divider);
+
+    for(const section of sections){
+        const schoolBoxSection = document.createElement("div");
+        schoolBoxSection.className = "school-box-section";
+        schoolBoxSection.innerHTML = school[section];
+        schoolBox.appendChild(schoolBoxSection);
+        let divider = document.createElement("div");
+        divider.className = "school-box-divider";
+        schoolBox.appendChild(divider);
+    }
+
+    return schoolBox;
 }
 
-async function getSports() {
-    const response = await fetch("/.netlify/functions/get-sports");
-
-    console.log("response:", response);
-
-    const data = await response.json();
-
-    console.log("sports data:", data);
-
-    return data.sports;
-}
 
