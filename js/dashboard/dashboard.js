0dd2328fb027775348abfc686206b0b1bde56f0b
ASoozay <asousa2021@gmail.com>
2026-06-20 15:07:10 -0700

    refactor html

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -10,6 +10,7 @@ netlifyIdentity.on("init", async user => {
 
     await fetchWriterData(user);
     await fetchUserSchoolInfo();
+    await loadModals();
 });
 // #endregion //
 
@@ -57,6 +58,28 @@ async function fetchWriterData(user) {
 
 };
 // #endregion //
+
+
+async function loadModals() {
+    const container = document.getElementById("modal-container");
+
+    const files = [
+        "/html/modals/add-season-modal.html",
+        "/html/modals/game-actions-modal.html",
+        "/html/modals/invoice-modal.html",
+        "/html/modals/search-games-modal.html",
+        "/html/modals/sports-modal.html",
+        "/html/modals/user-school-modal.html",
+        "/html/modals/writers-modal.html"
+    ];
+
+    for (const file of files) {
+        const response = await fetch(file);
+        container.innerHTML += await response.text();
+    }
+}
+
+
     
 
 
