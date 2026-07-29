d34e566c7969c72a1782cedc96bc0f9c29f63be5
The Daily Sports <sports@dailyuw.com>
2026-07-28 21:25:51 -0700

    Update add-season.js

--- js/settings/add-season.js
+++ js/settings/add-season.js
@@ -97,9 +97,9 @@ document.getElementById("add-season-confirm").addEventListener("click", async ()
         }
 
         if (date && !isNaN(date)) {
-            const yyyy = date.getUTCFullYear()
+            const yyyy = date.getUTCFullYear();
             const mm = String(date.getUTCMonth() + 1).padStart(2, "0");
-            const dd = String(date.getUTCDate().padStart(2, "0");
+            const dd = String(date.getUTCDate()).padStart(2, "0");
 
             row['Start Date'] = `${yyyy}-${mm}-${dd}`;
         }
